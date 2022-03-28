963c1c55fcacbb310ac4b7c1355928ba88497ad4
dls76 <75459253+dls76@users.noreply.github.com>
2022-03-27 22:12:50 -0300

    ajuste no código js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function show() {
 
     var data = new Date()
-    var diaSemana = data.getDay() /*Dia da semana*/
+    var diaSemana = data.getDay()
     var dia = data.getDate() 
     var mes = data.getMonth()
     var ano = data.getFullYear()
@@ -23,56 +23,52 @@ function show() {
     const pessoas = [
 
         {nome: "Douglas Leal dos Santos", dia: 20, mes: 2, ano: 1976,  funcao: "Diretor"}, 
-
         {nome: "Andressa Cristina Schmidt dos Santos", dia: 7, mes: 1, ano: "1985", funcao: "Orientadora"},
-        
         {nome: "Giovana Schmidt dos Santos", dia: 19, mes: 4, ano: 2005, funcao: "Estudante - 131"},
-        
         {nome: "Laura Schmidt dos Santos", dia: 2, mes: 6, ano: 2011, funcao: "Estudante - 63"},
 
-        {nome: "Douglas Leal dos Santos", dia: 27, mes: 2, ano: 1976, funcao: "Diretor"}
-
     ]
+
+    const aniversariantesDoDia = [] 
 
     // Data atual completa
     document.getElementById('hoje').innerHTML = `Hoje é ${diasDaSemana[diaSemana]}, ${dia} de ${meses[mes]} de ${ano}.`
 
-
-    /*Array de objetos - somente os aniversariantes do dia, com as propriedades nome, dia, mes, ano, funcao*/
-    // let aniversariantesDoDia = [] 
-
+    //Verifica quem faz aniversário hoje e inclui o objeto no array aniversariantesDoDia
     for ( i=0; i < pessoas.length; i++ ) {
-        
-        if (pessoas[i].dia == dia && pessoas[i].mes == mes) {
-        
-            document.getElementById('nome').innerHTML = pessoas[i].nome
-            document.getElementById('funcao').innerHTML = pessoas[i].funcao
-            break
 
-            } else {
-                window.location.href = 'http://dls76.github.io/D-Box/'
-            }
+        if (pessoas[i].dia == dia && pessoas[i].mes == mes) {
+
+          aniversariantesDoDia.push(pessoas[i]) 
+
+        }
+
+    } 
+
+    let n = aniversariantesDoDia.length
+
+    if (n > 0) {
+
+        for ( i = 0; i < aniversariantesDoDia.length; i++ ) {
+
+        let p = document.createElement("p")
+        p.innerHTML = aniversariantesDoDia[i].nome + " " + aniversariantesDoDia[i].funcao
+    
+        document.getElementById("nome").appendChild(p)
 
         } 
-       
+    
+    } else {
+        document.body.style.display = "none"
+        window.location="https://dls76.github.io/D-Box/"
+
+    }
+    
 }
+// document.getElementById('nome').innerHTML = aniversariantesDoDia[i].nome
+// document.getElementById('funcao').innerHTML = aniversariantesDoDia[i].funcao
 
-/*Até aqui funcionou*/
-            // aniversariantesDoDia.push(pessoas[i]) 
-            
-
-
-            // if (aniversariantesDoDia.length > 1) {
-
-            //     for (nome in aniversariantesDoDia) {
-            //         document.getElementById('nome').innerHTML = nome
-            //     }
-                // for ( j = 0; j < aniversariantesDoDia.length; j++ ) {
-
-                //     let nomesAnivDoDia = aniversariantesDoDia[j].nome
-
-                //     document.getElementById('nome').innerHTML = nomesAnivDoDia[j].nome                
-                
-                // }
-
-            // } else {
+function fechar() {
+    document.getElementById('conteiner').style.display = "none"
+    window.location="https://dls76.github.io/D-Box/"
+}
