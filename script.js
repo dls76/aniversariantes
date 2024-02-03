3f0933510d104ead057581d9c0caef15fa123d81
Douglas <75459253+dls76@users.noreply.github.com>
2024-02-03 16:16:42 -0300

    fev

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ function show() {
     var mes = data.getMonth()
     var ano = data.getFullYear()
 
-    
+    dia = 21
+    mes = 1
+
     const diasDaSemana = ["domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado"]
 
     const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
@@ -16,8 +18,19 @@ function show() {
     //DEZEMBRO
     {nome: "Valdene Neres de Sena Reis", dia: 12, mes: 11, ano: 1967, funcao: "Zeladora", foto:"img/dezembro/valdene.jpg"},
     {nome: "Carin Grime de Melo", dia: 13, mes: 11, ano: 1988, funcao: "Secretária", foto:"img/dezembro/carin.jpg"},
+
     {nome: "Glauton Vinicius de Andrade", dia: 27, mes: 11, ano: 1968, funcao: "Professor", foto:"img/dezembro/glauton.jpg"},
-    ]
+
+ // Fevereiro
+ {nome: "Andressa Cristina Schmidt dos Santos", dia: 7, mes: 1, ano: 1985, funcao: "Orientadora", foto:"img/fevereiro/andressa.jpg"},
+ {nome: "Stephany Moralles Vinharski", dia: 10, mes: 1, ano: 1995, funcao: "Psicóloga", foto:"img/fevereiro/stephanie.jpg"},
+ {nome: "Lucia Milczuk", dia: 16, mes: 1, ano: 1960, funcao: "Monitora", foto:"img/fevereiro/lucia.jpg"},
+ {nome: "Pamela Quintino Machado", dia: 17, mes: 1, ano: 1995, funcao: "Professora", foto:"img/fevereiro/pamela.jpg"},
+ {nome: "Julia Rocha de Magalhães", dia: 18, mes: 1, ano: 2000, funcao: "Recepcionista", foto:"img/fevereiro/julia.jpg"},
+ {nome: "Luciane Silverio Desbessel", dia: 21, mes: 1, ano: 1978, funcao: "Professora", foto:"img/fevereiro/luciane.jpg"},
+
+
+]
 
     const aniversariantesDoDia = [] 
 
